3e1a717d650a23614cc1053ea0bdb59b4a1ff3d1
Averniko <averniko@gmail.com>
2021-12-02 15:29:34 +0300

    add cleaner

--- backend/urls/tasks.py
+++ backend/urls/tasks.py
@@ -1,6 +1,15 @@
 from celery import app
+from django.contrib.sessions.models import Session
+from datetime import datetime
+
+from backend.settings import r
+from urls.models import Url
 
 
 @app.shared_task
 def clean():
-    pass
+    dt = datetime.now()
+    ids = Session.objects.filter(expire_date__lt=dt).values_list('pk', flat=True)
+    r.delete(list(ids.all()))
+    Url.objects.filter(session_key__in=ids).delete()
+
